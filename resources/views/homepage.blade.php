85f13b3adcce731baa903d826749f8b13bfebb7f
albertoBarl <alberto.barletta00@gmail.com>
2023-02-16 20:46:00 +0100

    added pre-header, created scss (scoped) files

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,20 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/js/app.js')
+    @vite('resources/js/app.js', 'resources/scss/app.scss')
 </head>
-<body class="bg-dark text-white">
+<body>
     <div>
         @include('partials.header')
-
     </div>
     <div>
         @include('partials.main')
-
     </div>
     <div>
         @include('partials.footer')
-
     </div>
 </body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,6 +8,46 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    PROVA HEADER
+    {{-- PRE-HEADER --}}
+    <div class="pre-header">
+        <div class="container d-flex justify-content-end gap-3 text-white">
+            <p>DC POWER VISA</p>
+            <div class="dropdown">
+                <p class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    ADDITION DC SITES
+                </p>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Option n</a></li>
+                  <li><a class="dropdown-item" href="#">Option n</a></li>
+                  <li><a class="dropdown-item" href="#">Option n</a></li>
+                </ul>
+              </div>
+        </div>
+    </div>
+
+{{-- HEADER --}}
+    <div>
+        <div class="container">
+            {{-- navabar --}}
+            <nav class="d-flex">
+                {{-- logo --}}
+                <div>
+                    <img src="{{ Vite::asset("resources/images/dc-logo.png") }}" alt="LOGO">
+                </div>
+                {{-- menu --}}
+                <div>PROVA MENU</div>
+                {{-- searchbar --}}
+                <div>
+                    <div class="form-control bg-transparent border-0">
+                        <input type="text" name="search" placeholder="Search...">
+                        <button class="btn btn-primary" type="submit">SEARCH</button>
+                    </div>
+                      
+                </div>
+            </nav>
+        </div>
+        {{-- jumbotron --}}
+        <div></div>
+    </div>
 </body>
 </html>
