0bd496ca3a1d54d4d45098651f67106394f52018
albertoBarl <alberto.barletta00@gmail.com>
2023-02-19 19:23:52 +0100

    fixed header style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
-<div id="header">
+<div id="full-header">
     {{-- PRE-HEADER --}}
-    <div class="pre-header">
+    <div id="pre-header">
         <div class="container d-flex justify-content-end gap-3 text-white">
             <p>DC POWER VISA</p>
             <div class="dropdown">
@@ -17,40 +17,36 @@
     </div>
 
     {{-- HEADER --}}
-    <div>
-        <div class="container p-4">
-            {{-- navabar --}}
-            <nav class="d-flex justify-content-between align-items-center">
+    <div id="header">
+        <nav class="container d-flex justify-content-between align-items-center py-3">
 
-                {{-- logo --}}
-                <div>
-                    <a href="/">
-                        <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="LOGO">
-                    </a>
+            {{-- logo --}}
+            <div id="logo">
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="LOGO">
+                </a>
+            </div>
+
+            {{-- menu --}}
+            <div id="nav">
+                <ul class="d-flex list-unstyled gap-3">
+                    @foreach ($menu as $item)
+                        <li><a class="text-black" href="{{ $item['url'] }}">{{ $item['voice'] }}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+
+            {{-- searchbar --}}
+            <div id="searchbar">
+                <div class="d-flex form-control bg-transparent">
+                    <input type="text" name="search" placeholder="Search..." class="border-0">
+                    <button class="btn" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
                 </div>
-
-                {{-- menu --}}
-                <div class="px-5 d-flex align-items-center">
-                    <ul class="d-flex list-unstyled gap-3 align-items-center">
-                        @foreach ($menu as $item)
-                            <li><a class="text-black" href="{{ $item['url'] }}">{{ $item['voice'] }}</a></li>
-                        @endforeach
-                    </ul>
-                </div>
-
-                {{-- searchbar --}}
-                <div id="searchbar">
-                    <div class="d-flex form-control bg-transparent">
-                        <input type="text" name="search" placeholder="Search..." class="border-0">
-                        <button class="btn" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                    </div>
-                </div>
-            </nav>
-        </div>
-
-        {{-- jumbotron --}}
-        <div class="jumbotron">
-            <img src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="not available">
-        </div>
+            </div>
+        </nav>
     </div>
+</div>
+{{-- jumbotron --}}
+<div class="jumbotron">
+    <img src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="not available">
 </div>
