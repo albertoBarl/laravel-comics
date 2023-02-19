aaf247576a86b79c56ce3e85b5da440a9e69c851
albertoBarl <alberto.barletta00@gmail.com>
2023-02-19 16:48:09 +0100

    added img and adv

--- resources/views/sections/singlecard.blade.php
+++ resources/views/sections/singlecard.blade.php
@@ -2,8 +2,31 @@
 
 @section('content')
     <div id="singleCard">
-        <div class="container">
-            <div class="card">prova {{ $singleCard['title'] }}</div>
+        <div id="sect1">
+            <div class="container">
+                <img class="card" src="{{ $singleCard['thumb'] }} " style="width: 200px">
+            </div>
+        </div>
+        <div id="sect2">
+            <div class="container d-flex justify-content-center">
+                <div class="row">
+                    <div class="col-6">
+                        <h3>{{ $singleCard['title'] }}</h3>
+                    </div>
+                    <div class="col-6">
+                        <label>ADVERTISEMENT</label>
+                        <a href="/">
+                            <img src="{{ Vite::asset('resources/images/adv.jpg') }}" alt="ADV">
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div id="sect3">
+            <div class="container">
+                PROVA SECT 3
+            </div>
         </div>
     </div>
 @endsection
